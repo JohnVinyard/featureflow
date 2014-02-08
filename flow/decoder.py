1e63a7a9eb31fceaf324c08b57c6e9560e1aeabb
jvinyard <jvinyard@localhost>
2014-02-08 09:39:10 -0600

    Adding code to setup.py

---
+++ flow/decoder.py
@@ -0,0 +1,3 @@
+'''
+decoders should be a callables that take a file-like object and return...anything
+'''
---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    '''
+    This is yanked from the setuptools documentation at 
+    http://packages.python.org/an_example_pypi_project/setuptools.html. It is
+    used to read the text from the README file.
+    '''
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+      name = 'flow',
+      version = '0.01',
+      url = 'http://www.johnvinyard.com',
+      author = 'John Vinyard',
+      author_email = 'john.vinyard@gmail.com',
+      long_description = read('README.md'),
+      install_requires = ['nose','unittest2']
+)

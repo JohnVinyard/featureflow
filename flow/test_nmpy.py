2046da73f2242c738bfbb0e00770d1a2ded9281c
jvinyard <jvinyard@localhost>
2015-01-27 21:29:02 -0600

    Factored out common logic and test code for numpy features

--- flow/test_nmpy.py
+++ flow/test_nmpy.py
@@ -19,9 +19,8 @@ class PassThrough(Node):
 	def _process(self,data):
 		yield data
 
-
-class NumpyTest(unittest2.TestCase):
-
+class BaseNumpyTest(object):
+	
 	def setUp(self):
 		if np is None: self.skipTest('numpy is not available')
 		Registry.register(IdProvider,UuidProvider())
@@ -29,18 +28,7 @@ class NumpyTest(unittest2.TestCase):
 		Registry.register(Database,InMemoryDatabase())
 		Registry.register(DataWriter,DataWriter)
 		Registry.register(DataReader,DataReaderFactory())
-
-	def _arrange(self,shape,dtype):
-		class Doc(BaseModel):
-			feat = NumpyFeature(PassThrough,store = True)
-
-		_id = Doc.process(feat = np.zeros(shape,dtype = dtype))
-		doc = Doc(_id)
-		arr = doc.feat
-		self.assertTrue(isinstance(arr,np.ndarray))
-		self.assertEqual(np.product(shape),arr.size)
-		self.assertEqual(dtype,arr.dtype)
-
+	
 	def test_can_store_and_retrieve_empty_array(self):
 		self._arrange((0,),np.uint8)
 
@@ -53,15 +41,20 @@ class NumpyTest(unittest2.TestCase):
 	def test_can_store_and_retrieve_multidimensional_float32_array(self):
 		self._arrange((5,10,11),np.float32)
 
-class StreamingNumpyTest(unittest2.TestCase):
+class GreedyNumpyTest(BaseNumpyTest,unittest2.TestCase):
 	
-	def setUp(self):
-		if np is None: self.skipTest('numpy is not available')
-		Registry.register(IdProvider,UuidProvider())
-		Registry.register(KeyBuilder,StringDelimitedKeyBuilder())
-		Registry.register(Database,InMemoryDatabase())
-		Registry.register(DataWriter,DataWriter)
-		Registry.register(DataReader,DataReaderFactory())
+	def _arrange(self,shape,dtype):
+		class Doc(BaseModel):
+			feat = NumpyFeature(PassThrough,store = True)
+
+		_id = Doc.process(feat = np.zeros(shape,dtype = dtype))
+		doc = Doc(_id)
+		arr = doc.feat
+		self.assertTrue(isinstance(arr,np.ndarray))
+		self.assertEqual(np.product(shape),arr.size)
+		self.assertEqual(dtype,arr.dtype)
+
+class StreamingNumpyTest(BaseNumpyTest,unittest2.TestCase):
 	
 	def _arrange(self,shape,dtype):
 		class Doc(BaseModel):
@@ -77,17 +70,3 @@ class StreamingNumpyTest(unittest2.TestCase):
 		self.assertTrue(isinstance(arr,np.ndarray))
 		self.assertEqual(np.product(shape),arr.size)
 		self.assertEqual(dtype,arr.dtype)
-
-	def test_can_store_and_retrieve_empty_array(self):
-		self._arrange((0,),np.uint8)
-
-	def test_can_store_and_retrieve_1d_float32_array(self):
-		self._arrange((33,),np.float32)
-
-	def test_can_store_and_retreive_multidimensional_uint8_array(self):
-		self._arrange((12,13),np.uint8)
-
-	def test_can_store_and_retrieve_multidimensional_float32_array(self):
-		self._arrange((5,10,11),np.float32)
-
-	

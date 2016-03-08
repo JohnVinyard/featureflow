7d365efe340da408d4f48b70c3ca92b47a80242b
John Vinyard <john.vinyard@gmail.com>
2016-03-07 20:30:46 -0600

    bump version

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ def read(fname):
 
 setup(
     name='featureflow',
-    version='0.1',
+    version='0.2',
     url='https://github.com/JohnVinyard/featureflow',
     author='John Vinyard',
     author_email='john.vinyard@gmail.com',
     long_description=read('README.md'),
     packages=['featureflow'],
-    download_url = 'https://github.com/jvinyard/featureflow/tarball/0.1',
+    download_url = 'https://github.com/jvinyard/featureflow/tarball/0.2',
     install_requires=['nose', 'unittest2', 'requests', 'lmdb']
 )

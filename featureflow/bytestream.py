61adcd6069780acd6d19a5b35e82ba78d10ff881
John Vinyard <john.vinyard@gmail.com>
2016-03-30 21:44:45 -0500

    Bytestream can now accept HTTP request objects, urls, filenames, and file-like objects, as well as the old HasUri interface

--- featureflow/bytestream.py
+++ featureflow/bytestream.py
@@ -13,14 +13,20 @@ class ByteStream(Node):
         super(ByteStream, self).__init__(needs=needs)
         self._chunksize = chunksize
 
+    def _generator(self, stream, content_length):
+        for chunk in chunked(stream, chunksize=self._chunksize):
+            yield StringWithTotalLength(chunk, content_length)
+
+    def _from_http_response(self, resp):
+        resp.raise_for_status()
+        content_length = int(resp.headers['Content-Length'])
+        return self._generator(resp.raw, content_length)
+
     def _handle_simple_get(self, data):
         parsed = urlparse(data)
         if parsed.scheme and parsed.netloc:
             resp = requests.get(data, stream=True)
-            resp.raise_for_status()
-            content_length = int(resp.headers['Content-Length'])
-            for chunk in chunked(resp.raw, chunksize=self._chunksize):
-                yield StringWithTotalLength(chunk, content_length)
+            return self._from_http_response(resp)
         else:
             raise ValueError
 
@@ -28,37 +34,36 @@ class ByteStream(Node):
         s = requests.Session()
         prepped = data.prepare()
         resp = s.send(prepped, stream=True)
-        resp.raise_for_status()
-        content_length = int(resp.headers['Content-Length'])
-        for chunk in chunked(resp.raw, chunksize=self._chunksize):
-            yield StringWithTotalLength(chunk, content_length)
+        return self._from_http_response(resp)
 
-    def _handle_local_file(self, data):
-        try:
-            with open(data, 'rb') as f:
-                content_length = int(os.path.getsize(data))
-                for chunk in chunked(f, chunksize=self._chunksize):
-                    yield StringWithTotalLength(chunk, content_length)
-        except TypeError:
-            content_length = data.seek(0, 2)
-            data.seek(0)
-            for chunk in chunked(data, chunksize=self._chunksize):
-                yield StringWithTotalLength(chunk, content_length)
+    def _handle_file_like_object(self, data):
+        content_length = data.seek(0, 2)
+        data.seek(0)
+        return self._generator(data, content_length)
+
+    def _handle_file(self, data):
+        with open(data, 'rb') as f:
+            content_length = int(os.path.getsize(data))
+            for chunk in self._generator(f, content_length):
+                yield chunk
+
+    def _get_strategy(self, data):
+        if isinstance(data, requests.Request):
+            return self._handle_http_request
+        if isinstance(data, str):
+            parsed = urlparse(data)
+            is_url = parsed.netloc and parsed.scheme
+            return self._handle_simple_get if is_url else self._handle_file
+        return self._handle_file_like_object
 
     def _process(self, data):
         try:
-            for chunk in self._handle_simple_get(data):
-                yield chunk
-            return
-        except (AttributeError, ValueError):
-            pass
-
-        try:
-            for chunk in self._handle_http_request(data.uri):
-                yield chunk
+            data = data.uri
         except AttributeError:
-            for chunk in self._handle_local_file(data.uri):
-                yield chunk
+            pass
+        strategy = self._get_strategy(data)
+        for chunk in strategy(data):
+            yield chunk
 
 
 class StringWithTotalLength(str):
@@ -108,12 +113,12 @@ class ByteStreamFeature(Feature):
             store=False,
             key=None,
             **extractor_args):
-        super(ByteStreamFeature, self).__init__( \
+        super(ByteStreamFeature, self).__init__(
                 extractor,
                 needs=needs,
                 store=store,
                 encoder=StringWithTotalLengthEncoder,
-                decoder=StringWithTotalLengthDecoder( \
+                decoder=StringWithTotalLengthDecoder(
                         chunksize=extractor_args['chunksize']),
                 key=key,
                 **extractor_args)
